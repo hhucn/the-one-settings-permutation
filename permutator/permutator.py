f88570f539ec7a2a993228abca64bbecf6730f10
Raphael Bialon <bialon@cs.uni-duesseldorf.de>
2018-07-25 11:18:47 +0200

    Add evenly split into different output files

--- permutator/permutator.py
+++ permutator/permutator.py
@@ -1,8 +1,8 @@
 import argparse
 import itertools
 import re
-from typing import List, Iterable
-
+from os.path import splitext
+from typing import List, Tuple
 
 MULTIPLE_VALUES_SETTING_REGEX = r"^[^#][^=]* = \[([^\]]+)\]"
 MULTIPLE_VALUES_SUB_REGEX = r"\[[^\]]+\]"
@@ -23,13 +23,13 @@ def read_possible_values_from_setting(settingsfile: str) -> List[List[str]]:
     return possible_values
 
 
-def generate_permutation_lists(possible_values: List[List[str]]) -> List[Iterable[str]]:
+def generate_permutation_lists(possible_values: List[List[str]]) -> List[Tuple[str]]:
     permutations = itertools.product(*possible_values)
 
     return list(zip(*permutations))
 
 
-def replace_possible_values_with_all_values(settings_out: str, settings_in: str, all_values: List[Iterable[str]]):
+def replace_possible_values_with_all_values(settings_out: str, settings_in: str, all_values: List[Tuple[str]]):
     matcher = re.compile(MULTIPLE_VALUES_SETTING_REGEX)
     replacer = re.compile(MULTIPLE_VALUES_SUB_REGEX)
     value_index = 0
@@ -45,23 +45,38 @@ def replace_possible_values_with_all_values(settings_out: str, settings_in: str,
             outfile.write(line)
 
 
+def split_lists_evenly(all_values: List[Tuple[str]], count: int) -> List[Tuple[str]]:
+    length = len(all_values[0])
+    step = length // count
+
+    for i in range(0, length, step):
+        yield [x[i:i + step] for x in all_values]
+
+
 def main():
     parser = argparse.ArgumentParser(
         description="Generate settings file(s) for The ONE containing permutations of possible values")
     parser.add_argument("infile", type=str, help="Settings file to read from")
     parser.add_argument("outfile", type=str, help="Settings file to generate")
-    parser.add_argument("-s", "--split", type=int, help="Split output settings permutations into x files")
+    parser.add_argument("-s", "--split", type=int, default=1, help="Split output settings permutations into x files")
     args = parser.parse_args()
 
     infile = args.infile
-    outfile = args.outfile
+    outfile = splitext(args.outfile)
     split_count = args.split
 
     possible_values = read_possible_values_from_setting(infile)
-    print(possible_values)
     all_values = generate_permutation_lists(possible_values)
-    print(all_values)
-    replace_possible_values_with_all_values(outfile, infile, all_values)
+
+    split_lists = split_lists_evenly(all_values, split_count)
+
+    for i in range(split_count):
+        if split_count == 1:
+            outfilename = "{}{}".format(*outfile)
+        else:
+            outfilename = "{}-{}{}".format(outfile[0], i, outfile[1])
+
+        replace_possible_values_with_all_values(outfilename, infile, next(split_lists))
 
 
 if __name__ == "__main__":
